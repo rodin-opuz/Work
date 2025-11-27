06f3d200845f05565565ba99469fec393470651b
Rodin Opuz <opuzrodin@gmail.com>
2025-11-27 11:11:32 +0000

    Create calculateAgain.java

---
+++ Year 1/Labwork/IN1007/Week 1/calculateAgain.java
@@ -0,0 +1,12 @@
+public class calculateAgain {
+    public static void main(String[] args) {
+        System.out.println(10 > 5);
+        System.out.println(false || (5 != 4));
+        System.out.println(10 == 5);
+        System.out.println(false && (5 != 4));
+        System.out.println(5 == 5);
+        System.out.println(!(30 % 3 == 0));
+        System.out.println(5 == 11 - 6);
+        System.out.println(0.3 - 0.2 == 0.2 - 0.1);
+    }
+}
