17c05520cefa3cceb56b939c4d32ed08bde1bade
Rodin Opuz <opuzrodin@gmail.com>
2025-11-27 10:56:04 +0000

    Create Calculate.java

---
+++ Year 1/Labwork/IN1007/Week 1/Calculate.java
@@ -0,0 +1,14 @@
+public class Calculate {
+    public static void main(String[] args) {
+        System.out.println(4 / 3);
+        System.out.println((4 - 3) * 5);
+        System.out.println(1.3 / 0);
+        System.out.println(4 / 3.0);
+        System.out.println(117 % 7);
+        System.out.println(-1.3 / 0);
+        System.out.println(0.3 - 0.2);
+        System.out.println(0 / 1.3);
+        System.out.println(0.2 - 0.1);
+        System.out.println(0 / 0);
+    }
+}
