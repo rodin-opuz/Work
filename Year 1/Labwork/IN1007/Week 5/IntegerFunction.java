1c32ceb95a83a872e04ebeca3a56c7c7dd3d9983
Rodin Opuz <opuzrodin@gmail.com>
2025-11-30 13:56:52 +0000

    Create IntegerFunction.java

---
+++ Year 1/Labwork/IN1007/Week 5/IntegerFunction.java
@@ -0,0 +1,10 @@
+public class IntegerFunction {
+    public static void main(String[] args) {
+        int result = compute(1245);
+        System.out.println(result);
+    }
+
+    public static int compute(int n) {
+        return 4 * n + 5;
+    }
+}
